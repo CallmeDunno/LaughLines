eb4ba0b831f99ea9d8cdee2c28f07e3ad5885b8a
CallmeDunno <dungworkit@gmail.com>
2024-04-04 00:42:32 +0700

    add Qr code generate feature

--- app/src/main/java/com/example/laughlines/ui/qr_code/QrCodeFragment.kt
+++ app/src/main/java/com/example/laughlines/ui/qr_code/QrCodeFragment.kt
@@ -1,79 +1,142 @@
 package com.example.laughlines.ui.qr_code
 
+import android.app.Dialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Context.VIBRATOR_SERVICE
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Handler
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.Gravity
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
-import androidx.viewpager2.widget.ViewPager2
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
 import com.example.laughlines.R
 import com.example.laughlines.base.BaseFragment
 import com.example.laughlines.databinding.FragmentQrCodeBinding
 import com.example.laughlines.utils.extensions.hide
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import com.example.laughlines.utils.extensions.show
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class QrCodeFragment : BaseFragment<FragmentQrCodeBinding>() {
     override val layoutId: Int = R.layout.fragment_qr_code
 
-    private val fragments = listOf(QrCodeScannerFragment(), QrCodeGeneratorFragment())
-
-    private val adapter by lazy {
-        QrCodeAdapter(childFragmentManager, lifecycle, fragments)
-    }
+    private lateinit var codeScanner: CodeScanner
 
     override fun initView() {
         super.initView()
-        binding.apply {
-            binding.toolbar.apply {
-                tvToolbar.text = getText(R.string.qr_code)
-                btnSettings.hide()
-            }
 
-            viewPager.adapter = adapter
-            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-                when (position) {
-                    0 -> {
-                        tab.text = getString(R.string.qr_code_scanner)
-                    }
-                    1 -> {
-                        tab.text = getString(R.string.qr_code_generator)
-                    }
-                }
-            }.attach()
-            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    gotoPage(position)
-                    super.onPageSelected(position)
-                }
-            })
+        binding.toolbar.apply {
+            tvToolbar.text = getString(R.string.qr_code_scanner)
+            btnSettings.hide()
         }
+        binding.apply {
+            btnGallery.paint.shader = LinearGradient(0f, 0f, 0f, btnGallery.lineHeight.toFloat(), intArrayOf(ContextCompat.getColor(requireContext(), R.color.jungle_green), ContextCompat.getColor(requireContext(), R.color.cyan_cornflower_blue)), null, Shader.TileMode.CLAMP)
+            btnMyQrCode.paint.shader = LinearGradient(0f, 0f, 0f, btnMyQrCode.lineHeight.toFloat(), intArrayOf(ContextCompat.getColor(requireContext(), R.color.jungle_green), ContextCompat.getColor(requireContext(), R.color.cyan_cornflower_blue)), null, Shader.TileMode.CLAMP)
+        }
+
+        initScanner()
     }
 
     override fun initAction() {
         super.initAction()
-        binding.apply {
-            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                override fun onTabSelected(tab: TabLayout.Tab) {
-                    when (tab.position) {
-                        0 -> {
-                            tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(requireContext(), R.color.jungle_green))
-                            tabLayout.setTabTextColors(ContextCompat.getColor(requireContext(), R.color.davy_grey), ContextCompat.getColor(requireContext(), R.color.jungle_green))
-                        }
-                        1 -> {
-                            tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(requireContext(), R.color.jungle_green))
-                            tabLayout.setTabTextColors(ContextCompat.getColor(requireContext(), R.color.davy_grey), ContextCompat.getColor(requireContext(), R.color.jungle_green))
-                        }
-                    }
-                }
+        binding.toolbar.btnBack.setOnClickListener { requireView().findNavController().popBackStack() }
+        binding.btnMyQrCode.setOnClickListener { requireView().findNavController().navigate(R.id.action_qrCodeFragment_to_qrCodeGeneratorFragment) }
+    }
 
-                override fun onTabUnselected(tab: TabLayout.Tab) {}
-                override fun onTabReselected(tab: TabLayout.Tab) {}
-            })
-            binding.toolbar.btnBack.setOnClickListener { requireView().findNavController().popBackStack() }
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+//        codeScanner.stopPreview()
+
+    }
+
+    private fun initScanner() {
+        codeScanner = CodeScanner(requireActivity(), binding.scannerView)
+        Handler().postDelayed({ binding.scannerView.visibility = View.VISIBLE }, 100)
+        codeScanner.decodeCallback = DecodeCallback { result ->
+            requireActivity().runOnUiThread {
+                codeScanner.stopPreview()
+                Handler().postDelayed({
+                                          val vibrator = requireContext().getSystemService(VIBRATOR_SERVICE) as Vibrator
+                                          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                                              vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE))
+                                          } else {
+                                              vibrator.vibrate(500)
+                                          }
+                                          binding.scannerView.isMaskVisible = false
+                                          notify(result.text)
+                                          showResultDialog(result.text)
+                                      }, 500)
+            }
         }
+
+        binding.scannerView.setOnClickListener { actionContinuePreview() }
     }
 
-    private fun gotoPage(currentItem: Int) {
-        binding.viewPager.currentItem = currentItem
+    private fun actionContinuePreview() {
+        codeScanner.startPreview()
+        binding.scannerView.isMaskVisible = true
     }
+
+    private fun showResultDialog(result: String): Dialog {
+        binding.scanning.hide()
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_qr_result)
+        val window = dialog.window!!
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val windowAttribute = window.attributes
+        windowAttribute.gravity = Gravity.CENTER
+        window.attributes = windowAttribute
+        val txtResult = dialog.findViewById<TextView>(R.id.txtResult)
+        val btnCopy = dialog.findViewById<ImageView>(R.id.btnCopy)
+        val btnClose = dialog.findViewById<ImageView>(R.id.btnClose)
+        btnClose.setOnClickListener { view1: View? ->
+            dialog.dismiss()
+            actionContinuePreview()
+        }
+        txtResult.text = result
+        btnCopy.setOnClickListener { view1: View? ->
+            val clipboard = requireActivity().getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("label", result)
+            if (clipboard != null) {
+                clipboard.setPrimaryClip(clip)
+//                btnCopy.setImageResource(R.drawable.ic_check)
+                btnCopy.imageTintList = ColorStateList.valueOf(Color.parseColor("#1CCF7A"))
+                btnCopy.isClickable = false
+            }
+//            notify(getString(R.string.copy_successful))
+        }
+        dialog.setCancelable(false)
+
+        dialog.setOnDismissListener {
+            binding.scanning.show()
+            onResume()
+        }
+
+        dialog.show()
+        return dialog
+    }
+
 }
--- app/src/main/java/com/example/laughlines/ui/qr_code/QrCodeGeneratorFragment.kt
+++ app/src/main/java/com/example/laughlines/ui/qr_code/QrCodeGeneratorFragment.kt
@@ -1,9 +1,16 @@
 package com.example.laughlines.ui.qr_code
 
+import android.graphics.Color
+import android.util.Log
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import androidx.navigation.findNavController
 import com.example.laughlines.R
 import com.example.laughlines.base.BaseFragment
 import com.example.laughlines.databinding.FragmentQrCodeGeneratorBinding
+import com.google.zxing.WriterException
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class QrCodeGeneratorFragment : BaseFragment<FragmentQrCodeGeneratorBinding>() {
@@ -11,11 +18,20 @@ class QrCodeGeneratorFragment : BaseFragment<FragmentQrCodeGeneratorBinding>() {
 
     override fun initView() {
         super.initView()
+        val qrgEncoder = QRGEncoder("Dung dep zai vl", null, QRGContents.Type.TEXT, 300)
+        qrgEncoder.colorBlack = Color.WHITE
+        qrgEncoder.colorWhite = Color.BLACK
+        try {
+            val bitmap = qrgEncoder.bitmap
+            binding.imgQr.setImageBitmap(bitmap)
+        } catch (e: WriterException) {
+            Log.v("Dunno", e.toString())
+        }
     }
 
     override fun initAction() {
         super.initAction()
-
+        binding.toolbar.btnBack.setOnClickListener { requireView().findNavController().popBackStack() }
     }
 
 }
